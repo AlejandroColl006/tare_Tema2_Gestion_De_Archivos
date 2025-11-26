b8b0e35b0579dff757f02152f12808aea84373fd
Your Name <acolllen@myuax.com>
2025-11-26 09:46:16 +0100

    Creación de las clases ejercicio2.c, creación del ejemplo.txt encontrado en el cmake-debug, resuelvo el ejercicio1.c y en el README.md pongo la explicación del ejercicio1.c, es decir, lo comento.

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,3 +1,27 @@
-//
-// Created by Alex on 15/11/2025.
-//
+#include <stdio.h>
+
+int main() {
+    FILE *archivo;
+
+    archivo = fopen("ejemplo.txt", "w");
+    if (archivo == NULL) {
+        perror ("Error al abrir el archivo. \n");
+        return 1;
+    }
+
+    fprintf(archivo, "Hola, este es un saludo desde otro archivo. \n");
+    fclose(archivo);
+
+    archivo = fopen("ejemplo.txt", "r");
+    if (archivo == NULL) {
+        perror ("Error al abrir el archivo. \n");
+        return 1;
+    }
+
+    char buffer[100];
+    fgets(buffer, sizeof(buffer), archivo);
+    printf("Contenido del archivo: %s", buffer);
+
+    fclose(archivo);
+    return 0;
+}
--- ejercicio2.c
+++
@@ -1,3 +0,0 @@
-//
-// Created by Alex on 15/11/2025.
-//
